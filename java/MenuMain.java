5b88edac4c2bc196e8cf3ce4a9d4d1ad75af5cb3
Kashif Brown Jr <kashifbrownjr@gmail.com>
2024-03-31 17:02:15 -0400

    Implemented cube class, cleaned whitespace.

--- java/MenuMain.java
+++ java/MenuMain.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+ 
 
 
 public class MenuMain {
@@ -14,29 +15,26 @@ public class MenuMain {
             input = scan.nextLine();
             
             /*
-             * Volume of a cube is v=s^3. The lenght, width and height are all 
-             * equal, therefore only one side is needed to solve the equation.
-             * 
-             * TODO: create and import new class called Cube Volume and use this 
-             * class to calculate/return value of a cube's volume.
+             * Volume of a cube is v=s^3. The length, width and height are all 
+             * equal, therefore only one side is n
+             *   eeded to solve the equation.
              */
             switch (input){
-                case "1":
-                    
-                    int num;
-                    System.out.print("Cube Calculator\n"+line2+"\n"+"Enter a number: ");
-                    num = scan.nextInt();
-
-                    //import math and use exp to do this properly..
-                    double v = num * num * num;
-
-                    System.out.println("\nThe volume of the cube is: "+v+"^m3"+line2);
-
+                case "":
+                    System.out.println("No input detected. Try again!");;
                     break;
+                case "1":   
+                    cube cube = new cube();
+                    System.out.print("Cube Calculator\n"+line2+"\n"+"Enter a number: ");
+                    double side = scan.nextDouble();
+                    double volume = cube.cubeVolume(side);
+                    System.out.println("\nThe volume of the cube is: "+volume+"^m3");
+                    input = "";
+                    break;
+
                 default:
                     System.out.println("invalid input. Try Again!");
-            }
-            
+            }//end of switch 
         }
         scan.close();
     }
---
+++ java/cube.java
@@ -0,0 +1,8 @@
+public class cube {
+    public double cubeVolume(double side){
+        double v = Math.pow(side, 3.0);
+        return v;
+    }
+}
+
+
