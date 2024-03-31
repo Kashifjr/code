c101573e7a5b37db2c19d81767d0c966becd9b0c
Kashif Brown Jr <kashifbrownjr@gmail.com>
2024-03-31 17:57:30 -0400

    fixed break while loop bug

--- java/MenuMain.java
+++ java/MenuMain.java
@@ -10,19 +10,16 @@ public class MenuMain {
         String line2 = "---------------------------------";
         String options = "Cube Volume Calc.......1\n";
 
-        while(input != "q"){
+        while(!input.equals("q")){
             System.out.println(topLine+"\n"+"Menu"+"\n"+line2+"\n"+options);
-            input = scan.nextLine();
-            
+            input = scan.next();
+                       
             /*
              * Volume of a cube is v=s^3. The length, width and height are all 
-             * equal, therefore only one side is n
-             *   eeded to solve the equation.
+             * equal, therefore only one side is
+             * needed to solve the equation.
              */
             switch (input){
-                case "":
-                    System.out.println("No input detected. Try again!");;
-                    break;
                 case "1":   
                     cube cube = new cube();
                     System.out.print("Cube Calculator\n"+line2+"\n"+"Enter a number: ");
@@ -31,7 +28,9 @@ public class MenuMain {
                     System.out.println("\nThe volume of the cube is: "+volume+"^m3");
                     input = "";
                     break;
-
+                case "q":
+                    System.out.println(line2+"\nThanks for using the Program!\n"+topLine);
+                    break;
                 default:
                     System.out.println("invalid input. Try Again!");
             }//end of switch 
