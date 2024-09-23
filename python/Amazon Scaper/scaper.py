ee28e9ed8ddbb45aa5b6ca3ed0719d2868a49bb1
Kashif Brown Jr <kashifbrownjr@gmail.com>
2024-09-22 23:36:54 -0400

    Created Amazon price scaper application

---
+++ python/Amazon Scaper/scaper.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+# url of item to check price of
+URL = 'https://www.bestbuy.com/site/jvc-6-8-bluetooth-digital-media-dm-receiver-with-rear-camera-input-and-parking-guidelines-black/6544996.p?skuId=6544996'
+
+amazon = 'https://www.amazon.com/Zeee-Batteries-Connector-Helicopter-Quadcopter/dp/B0972RKJDS/ref=sr_1_1_sspa?dib=eyJ2IjoiMSJ9.5hKdpgWW2pJIzFBYoEmgISa8YfivcQIkJiiYH7x2uDDVHLid7O8FMbysu-IiGrTV9hhRztodkGYm1v_c0SOzA7sBpslljoajW1kCj6avS_PDuNKYYzuJpKRyp4mPv0USdpLwJTpxNjcMO9gtva-c87XNwc4VDWOStPAnDmwZX23MhzEFXPQhh_fY6AiIyXZ52Zeqn_nuGLBpAmiUckFK2n0x80Z715O2l6dRF8lLfshUfvs0z8KWsREoD_7WpLEC7UOd5UkGRmehhRPqj68_d3R9ptdWKicIisOgPpxyFQA.zND7HOSj5xD-rF-xX42SjraeFo7oWVYG6zbs4B1enRM&dib_tag=se&keywords=3s+lipo+battery&qid=1727061514&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1'
+
+# google 'my user agent'
+# the 'accept-language' will prevent Amazon from blocking this request
+# b/c they block when detecting a bot
+headers = {
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0',
+    'accept-language': 'en-US,en;q=0.9',
+}
+
+page = requests.get(amazon, headers=headers)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+#print(soup.find(id="productTitle").get_text().strip())
+
+title = soup.find(id="productTitle").get_text().strip()
+
+whole = soup.find(attrs="a-price-whole").get_text()
+decimal = soup.find(attrs="a-price-fraction").get_text()
+price = whole + decimal
+
+
+print(title+"\n"+price)
