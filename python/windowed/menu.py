b1bdde20e53c381893a88b0fe0f4f8ae4b09f78a
Kashif Brown Jr <kashifbrownjr@gmail.com>
2025-11-18 19:33:08 -0500

    initial python window gui setup and test

---
+++ python/windowed/menu.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+
+def button_click():
+    user_input = entry.get()
+    #label.config(text=user_input)
+
+# create the main window
+window = tk.Tk()
+window.title("My first GUI")
+window.geometry("300x200")
+
+# label to display text
+label = tk.Label(window, width=30)
+label.pack(pady=10)
+label.config(text="Choose an Option:")
+
+
+# text field
+entry = tk.Entry(window, width=30)
+entry.pack(pady=10)
+
+# create button and attatch func.
+button = tk.Button(window,text="Submit", command=button_click)
+button.pack(pady=10)
+
+
+# start Tkinter event loop
+window.mainloop()
